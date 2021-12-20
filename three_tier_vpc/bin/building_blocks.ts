679ea8f1c6105b9fe1846453327cbe6cf2fc8b91
Bhavik Shah <shahbhavik002@gmail.com>
2021-12-19 19:49:24 -0800

    Added Service Catalog and Route 53 private zone

--- three_tier_vpc/bin/building_blocks.ts
+++ three_tier_vpc/bin/building_blocks.ts
@@ -1,29 +1,28 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { BuildingBlocksStack } from '../lib/building_blocks-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import * as servicecatalog from "@aws-cdk/aws-servicecatalog";
+
+import { BuildingBlocksStack } from "../lib/building_blocks-stack";
+// import { ThreeTierVPCProduct } from "../lib/building_blocks-stack";
 
 const app = new cdk.App();
-new BuildingBlocksStack(app, 'BuildingBlocksStack', {
+new BuildingBlocksStack(app, "BuildingBlocksStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
-
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
-
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
 
 const appName = app.node.tryGetContext("appName");
 const owner = app.node.tryGetContext("owner");
 const environment = app.node.tryGetContext("environment");
-
 
 // Tags
 cdk.Tags.of(app).add("application", appName);
--- three_tier_vpc/lib/building_blocks-stack.ts
+++ three_tier_vpc/lib/building_blocks-stack.ts
@@ -1,43 +1,132 @@
-import * as ec2 from '@aws-cdk/aws-ec2';
-import * as cdk from '@aws-cdk/core';
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as cdk from "@aws-cdk/core";
+import * as iam from "@aws-cdk/aws-iam";
+import * as route53 from "@aws-cdk/aws-route53";
+
+import * as servicecatalog from "@aws-cdk/aws-servicecatalog";
 
 export class BuildingBlocksStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor(scope: cdk.Construct, id: string, props: cdk.StackProps) {
+    super(scope, id);
 
-    const vpcCidr = this.node.tryGetContext("vpcCidr");
-    const publicCidrMask = this.node.tryGetContext("publicCidrMask");
-    const privateCidrMask = this.node.tryGetContext("privateCidrMask");
-    const isolatedCidrMask = this.node.tryGetContext("isolatedCidrMask");
-    const natGateways = this.node.tryGetContext("natGateways");
-    const maxAzs = this.node.tryGetContext("maxAzs");
+    // const vpcCidr = this.node.tryGetContext("vpcCidr");
+
     const appName = this.node.tryGetContext("appName");
 
-    const vpc = new ec2.Vpc(
-      this, 
-      appName.concat("-vpc"), 
+    class ThreeTierVPCProduct extends servicecatalog.ProductStack {
+      constructor(scope: cdk.Construct, id: string) {
+        super(scope, id);
+
+        // TODO: Implement this.
+        // const vpcCidr = new cdk.CfnParameter(this, "vpcCidr", {
+        //   type: "String",
+        //   description: "The CIDR of the VPC",
+        //   default: "10.0.0.0/16",
+        // });
+
+        const natGateways = new cdk.CfnParameter(this, "natGateways", {
+          type: "Number",
+          description: "The number of Nat Gateways to attach",
+          default: 1,
+        });
+
+        const maxAzs = new cdk.CfnParameter(this, "maxAzs", {
+          type: "Number",
+          description: "The number of AZs to create subnets in",
+          default: "3",
+        });
+
+        const publicCidrMask = new cdk.CfnParameter(this, "publicCidrMask", {
+          type: "Number",
+          description: "The CIDR mask of public subnets",
+          default: "24",
+        });
+
+        const privateCidrMask = new cdk.CfnParameter(this, "privateCidrMask", {
+          type: "Number",
+          description: "The CIDR mask of private subnets",
+          default: "24",
+        });
+
+        const isolatedCidrMask = new cdk.CfnParameter(
+          this,
+          "isolatedCidrMask",
+          {
+            type: "Number",
+            description: "The CIDR mask of isolated subnets",
+            default: "28",
+          }
+        );
+
+        const vpc = new ec2.Vpc(this, appName.concat("-vpc"), {
+          cidr: "10.0.0.0/16", // Fix this later.
+          natGateways: natGateways.valueAsNumber,
+          maxAzs: maxAzs.valueAsNumber,
+          subnetConfiguration: [
+            {
+              name: appName.concat("public-subnet"),
+              subnetType: ec2.SubnetType.PUBLIC,
+              cidrMask: publicCidrMask.valueAsNumber,
+            },
+            {
+              name: appName.concat("private-subnet"),
+              subnetType: ec2.SubnetType.PRIVATE,
+              cidrMask: privateCidrMask.valueAsNumber,
+            },
+            {
+              name: appName.concat("isolated-subnet"),
+              subnetType: ec2.SubnetType.ISOLATED,
+              cidrMask: isolatedCidrMask.valueAsNumber,
+            },
+          ],
+        });
+
+        const privateHostedZone = new route53.PrivateHostedZone(
+          this,
+          "HostedZone",
+          {
+            zoneName: "private.domain.com",
+            vpc, // At least one VPC has to be added to a Private Hosted Zone.
+          }
+        );
+      }
+    }
+
+    const threeTierVPC = new servicecatalog.CloudFormationProduct(
+      this,
+      "ThreeTierVPC",
       {
-        cidr: vpcCidr,
-        natGateways: natGateways,
-        maxAzs: maxAzs,
-        subnetConfiguration: [
+        productName: "ThreeTierVPC",
+        owner: "Bhavik Shah",
+        productVersions: [
           {
-            name: appName.concat('public-subnet'),
-            subnetType: ec2.SubnetType.PUBLIC,
-            cidrMask: publicCidrMask,
-          },
-          {
-            name: appName.concat('private-subnet'),
-            subnetType: ec2.SubnetType.PRIVATE,
-            cidrMask: privateCidrMask,
-          },
-          {
-            name: appName.concat('isolated-subnet'),
-            subnetType: ec2.SubnetType.ISOLATED,
-            cidrMask: isolatedCidrMask,
+            productVersionName: "v0.1",
+            cloudFormationTemplate:
+              servicecatalog.CloudFormationTemplate.fromProductStack(
+                new ThreeTierVPCProduct(this, "ThreeTierVPCProduct")
+              ),
           },
         ],
       }
     );
+
+    const networkPortfolio = new servicecatalog.Portfolio(
+      this,
+      "NetworkPortfolio",
+      {
+        displayName: "NetworkPortfolio",
+        providerName: "Bhavik Shah",
+      }
+    );
+
+    networkPortfolio.addProduct(threeTierVPC);
+
+    const role = iam.Role.fromRoleArn(
+      this,
+      "ExistingAdminRole",
+      "arn:aws:iam::899456967600:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AWSAdministratorAccess_30f517a3940f0385"
+    );
+
+    networkPortfolio.giveAccessToRole(role);
   }
 }
